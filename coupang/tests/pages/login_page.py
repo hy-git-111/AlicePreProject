b35b11856d52aa26ef63d5a5c858772fc62affd4
hy-git-111 <hyeyoung.k111@gmail.com>
2025-03-17 22:04:13 +0900

    login_page 기능 수정
    1. 변수 위치 수정
    2. 대기시간 수정

--- coupang/tests/pages/login_page.py
+++ coupang/tests/pages/login_page.py
@@ -11,7 +11,11 @@ class LoginPage:
     url = "https://www.coupang.com/"
     #url = "https://login.coupang.com/login/login.pang?rtnUrl=https%3A%2F%2Fwww.coupang.com%2Fnp%2Fpost%2Flogin%3Fr%3Dhttps%253A%252F%252Fwww.coupang.com%252F"
     # SEARCH_BUTTON_ID = "headerSearchBtn"
-    random_wait = random.randrange(1,11)
+    random_wait = random.randrange(30,61)
+    EMAIL_ID = "login-email-input"
+    PASSWORD_ID = "login-password-input"
+    LOGIN_BTN_CLASS_NAME = "_loginSubmitButton"
+
 
     # 객체 인스턴스화를 위한 세팅, 파이테스트의 'driver'를 받아 driver 객체에 넣는다.
     def __init__(self, driver: WebDriver):
@@ -34,21 +38,18 @@ class LoginPage:
         
     # 로그인
     def input_user_data(self):
-        EMAIL_ID = "login-email-input"
-        PASSWORD_ID = "login-password-input"
-        LOGIN_BTN_CLASS_NAME = "_loginSubmitButton"
 
         clipboard.copy(user_data.EMAIL)
-        email_input_box = self.driver.find_element(By.ID, EMAIL_ID)
+        email_input_box = self.driver.find_element(By.ID, self.EMAIL_ID)
         email_input_box.click()
         clipboard.paste()
         time.sleep(self.random_wait)
 
         clipboard.copy(user_data.PASSWORD)
-        password_input_box = self.driver.find_element(By.ID, PASSWORD_ID)
+        password_input_box = self.driver.find_element(By.ID, self.PASSWORD_ID)
         password_input_box.click()
         clipboard.paste()
         time.sleep(3)
 
-        login_btn = self.driver.find_element(By.CLASS_NAME, LOGIN_BTN_CLASS_NAME)
+        login_btn = self.driver.find_element(By.CLASS_NAME, self.LOGIN_BTN_CLASS_NAME)
         login_btn.send_keys(Keys.ENTER)
